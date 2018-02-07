87a0ee3d936ca17db82fa14c11b73b29d2fe0677
losizm <losizm@gmail.com>
2018-02-07 00:03:59 -0500

    Moved BodyParsing to its own file

--- src/main/scala/scamper/BodyParser.scala
+++ src/main/scala/scamper/BodyParser.scala
@@ -4,8 +4,7 @@ import bantam.nx.io._
 
 import com.typesafe.config.ConfigFactory
 
-import java.io.{ File, InputStream, SequenceInputStream }
-import java.util.zip.{ GZIPInputStream, InflaterInputStream }
+import java.io.{ File, InputStream }
 
 import scala.collection.mutable.ArrayBuffer
 
@@ -75,44 +74,6 @@ object BodyParser {
    */
   def file(dest: File, maxLength: Long): BodyParser[File] =
     new FileBodyParser(dest, maxLength, maxBufferSize)
-}
-
-/** A mixin that provides access to decoded message body. */
-trait BodyParsing {
-  /** Maximum body length allowed */
-  def maxLength: Long
-
-  /** Maximum buffer size allowed */
-  def maxBufferSize: Int
-
-  /**
-   * Provides decoded input stream to message body. That is, the input stream
-   * passed to handler is uncompressed and dechunked.
-   *
-   * @param message HTTP message
-   * @param f input stream handler
-   *
-   * @return value returned from handler
-   */
-  def withInputStream[T](message: HttpMessage)(f: InputStream => T): T =
-    message.body.withInputStream { in =>
-      val dechunked =
-        if (isChunked(message)) chunkInputStream(in)
-        else new BoundInputStream(in, message.headerValue("Content-Length").toLong)
-
-      message.contentEncoding.getOrElse("identity") match {
-        case "gzip"     => f(new GZIPInputStream(dechunked))
-        case "deflate"  => f(new InflaterInputStream(dechunked))
-        case "identity" => f(dechunked)
-        case encoding   => throw new HttpException(s"Unsupported content encoding: $encoding")
-      }
-    }
-
-  private def chunkInputStream(in: InputStream) =
-    new SequenceInputStream(new ChunkEnumeration(in, maxBufferSize, maxLength))
-
-  private def isChunked(message: HttpMessage): Boolean =
-    message.isChunked && !message.getHeaderValue("X-Scamper-Decoding").exists(_.contains("chunked"))
 }
 
 private class ByteArrayBodyParser(val maxLength: Long) extends BodyParser[Array[Byte]] with BodyParsing {
---
+++ src/main/scala/scamper/BodyParsing.scala
@@ -0,0 +1,43 @@
+package scamper
+
+import java.io.{ InputStream, SequenceInputStream }
+import java.util.zip.{ GZIPInputStream, InflaterInputStream }
+
+/** A mixin that provides access to decoded message body. */
+trait BodyParsing {
+  /** Maximum body length allowed */
+  def maxLength: Long
+
+  /** Maximum buffer size allowed */
+  def maxBufferSize: Int
+
+  /**
+   * Provides decoded input stream to message body. That is, the input stream
+   * passed to handler is uncompressed and dechunked.
+   *
+   * @param message HTTP message
+   * @param f input stream handler
+   *
+   * @return value returned from handler
+   */
+  def withInputStream[T](message: HttpMessage)(f: InputStream => T): T =
+    message.body.withInputStream { in =>
+      val dechunked =
+        if (isChunked(message)) chunkInputStream(in)
+        else new BoundInputStream(in, message.headerValue("Content-Length").toLong)
+
+      message.contentEncoding.getOrElse("identity") match {
+        case "gzip"     => f(new GZIPInputStream(dechunked))
+        case "deflate"  => f(new InflaterInputStream(dechunked))
+        case "identity" => f(dechunked)
+        case encoding   => throw new HttpException(s"Unsupported content encoding: $encoding")
+      }
+    }
+
+  private def chunkInputStream(in: InputStream) =
+    new SequenceInputStream(new ChunkEnumeration(in, maxBufferSize, maxLength))
+
+  private def isChunked(message: HttpMessage): Boolean =
+    message.isChunked && !message.getHeaderValue("X-Scamper-Decoding").exists(_.contains("chunked"))
+}
+
