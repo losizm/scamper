47fb46e4c3fcc2226245d4faa91398f2f1df217f
Carlos Conyers <carlos.conyers@hotmail.com>
2018-10-28 03:18:57 -0400

    Added HttpClient and removed client methods from URLType

---
+++ src/main/scala/scamper/HttpClient.scala
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2018 Carlos Conyers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package scamper
+
+import java.net.{ HttpURLConnection, URI, URL }
+import java.time.{ LocalDate, LocalDateTime, OffsetDateTime }
+
+import scala.annotation.tailrec
+import scala.util.Try
+
+import extensions._
+
+/** HTTP client */
+object HttpClient {
+  /**
+   * Sends request and passes response to supplied handler.
+   *
+   * To make effective use of this method, either the Host header must be set,
+   * or the request URI must be absolute. Also note that if the request URI is
+   * absolute, its scheme is overridden in accordance to {@code secure}.
+   *
+   * @param request HTTP request
+   * @param secure specifies whether to use HTTPS protocol
+   * @param handler response handler
+   *
+   * @return value from supplied handler
+   */
+  def send[T](request: HttpRequest, secure: Boolean = false)(handler: HttpResponse => T): T = {
+    val scheme = if (secure) "https" else "http"
+    val uri = request.uri.toURI
+    val host = getHost(uri, request.getHeaderValue("Host"))
+    val headers = Header("Host", host) +: request.headers.filterNot(_.key.equalsIgnoreCase("Host"))
+
+    uri.toURL(scheme, host).withConnection { implicit conn =>
+      conn.setRequestMethod(request.method.name)
+      request.headers.foreach(header => conn.addRequestProperty(header.key, header.value))
+
+      if (!request.body.isKnownEmpty)
+        writeBody(request.body)
+
+      val response = HttpResponse(
+        StatusLine.parse(conn.getHeaderField(0)),
+        getResponseHeaders(),
+        getResponseBody()
+      )
+
+      handler(response)
+    }
+  }
+
+  private def getHost(uri: URI, default: => Option[String]): String =
+    Option(uri.getAuthority).orElse(default).getOrElse(throw HeaderNotFound("Host"))
+
+  private def writeBody(body: Entity)(implicit conn: HttpURLConnection): Unit = {
+    conn.setDoOutput(true)
+
+    body.length match {
+      case Some(length) =>
+        conn.setRequestProperty("Content-Length", length.toString)
+        conn.setFixedLengthStreamingMode(length)
+
+      case None =>
+        conn.setRequestProperty("Transfer-Encoding", "chunked")
+        conn.setChunkedStreamingMode(8192)
+    }
+
+    body.withInputStream { in =>
+      val out = conn.getOutputStream
+      val buf = new Array[Byte](8192)
+      var len = 0
+
+      while ({ len = in.read(buf); len != -1 })
+        out.write(buf, 0, len)
+    }
+  }
+
+  private def getResponseHeaders()(implicit conn: HttpURLConnection): Seq[Header] = {
+    val headers = getResponseHeaders(1, Nil)
+
+    if ("chunked".equalsIgnoreCase(conn.getHeaderField("Transfer-Encoding")))
+      headers :+ Header.parse("X-Scamper-Transfer-Decoding: chunked")
+    else headers
+  }
+
+  @tailrec
+  private def getResponseHeaders(keyIndex: Int, headers: Seq[Header])(implicit conn: HttpURLConnection): Seq[Header] =
+    conn.getHeaderFieldKey(keyIndex) match {
+      case null => headers
+      case key  => getResponseHeaders(keyIndex + 1, headers :+ Header(key, conn.getHeaderField(keyIndex)))
+    }
+
+  private def getResponseBody()(implicit conn: HttpURLConnection): Entity =
+    Entity { () =>
+      if (conn.getResponseCode < 400) conn.getInputStream
+      else conn.getErrorStream
+    }
+}
--- src/main/scala/scamper/extensions/package.scala
+++ src/main/scala/scamper/extensions/package.scala
@@ -15,40 +15,13 @@
  */
 package scamper
 
-import java.time.{ LocalDate, LocalDateTime, OffsetDateTime }
 import java.net.{ HttpURLConnection, URI, URL, URLDecoder, URLEncoder }
-import scala.annotation.tailrec
+import java.time.{ LocalDate, LocalDateTime, OffsetDateTime }
+
 import scala.util.Try
 
 /** Contains type classes of HttpRequest, String, URI, and URL. */
 package object extensions {
-  /** Type class of [[HttpRequest]]. */
-  implicit class HttpRequestType(val request: HttpRequest) extends AnyVal {
-    /**
-     * Sends request and passes response to supplied handler.
-     *
-     * To make effective use of this method, either the Host header must be set,
-     * or the request URI must be absolute. Also note that if the request URI is
-     * absolute, its scheme is overridden in accordance to {@code secure}.
-     *
-     * @param secure specifies whether to use HTTPS protocol
-     * @param f response handler
-     *
-     * @return value returned from supplied handler
-     */
-    def send[T](secure: Boolean = false)(f: HttpResponse => T): T = {
-      val scheme = if (secure) "https" else "http"
-      val uri = request.uri.toURI
-      val host = getHost(uri)
-      val headers = Header("Host", host) +: request.headers.filterNot(_.key.equalsIgnoreCase("Host"))
-
-      uri.toURL(scheme, host).request(request.method.name, headers, Some(request.body))(f)
-    }
-
-    private def getHost(uri: URI): String =
-      Option(uri.getAuthority).orElse(request.getHeaderValue("Host")).getOrElse(throw HeaderNotFound("Host"))
-  }
-
   /** Type class of {@code String}. */
   implicit class StringType(val string: String) extends AnyVal {
     /**
@@ -184,180 +157,20 @@ package object extensions {
     /**
      * Opens HTTP connection and passes it to supplied handler.
      *
-     * The connection is disconnected upon handler's return.
+     * The connection is disconnected upon handler return.
      *
-     * @param f connection handler
+     * @param handler connection handler
      *
-     * @return value returned from supplied handler
+     * @return value from supplied handler
      */
-    def withConnection[T](f: HttpURLConnection => T): T = {
-      val conn = url.openConnection()
-
-      try f(conn.asInstanceOf[HttpURLConnection])
-      finally Try(conn.asInstanceOf[HttpURLConnection].disconnect())
+    def withConnection[T](handler: HttpURLConnection => T): T = {
+      val conn = url.openConnection().asInstanceOf[HttpURLConnection]
+      try handler(conn)
+      finally Try(conn.disconnect())
     }
-
-    /**
-     * Sends HTTP request and passes response to supplied handler.
-     *
-     * @param method request method
-     * @param headers request headers
-     * @param body request entity body
-     * @param f response handler
-     *
-     * @return value returned from supplied handler
-     */
-    def request[T](method: String, headers: Seq[Header] = Nil, body: Option[Entity] = None)(f: HttpResponse => T): T =
-      withConnection { conn =>
-        conn.setRequestMethod(method)
-        headers.foreach(header => conn.addRequestProperty(header.key, header.value))
-
-        body.filterNot(_.isKnownEmpty).foreach { entity =>
-          conn.setDoOutput(true)
-          writeBody(conn, entity)
-        }
-
-        val statusLine = StatusLine.parse(conn.getHeaderField(0))
-        val response = HttpResponse(statusLine, getHeaders(conn), getBody(conn))
-
-        f(response)
-      }
-
-    /**
-     * Sends GET request and passes response to supplied handler.
-     *
-     * @param headers request headers
-     * @param f response handler
-     *
-     * @return value returned from supplied handler
-     */
-    def get[T](headers: Header*)(f: HttpResponse => T): T =
-      request("GET", headers)(f)
-
-    /**
-     * Sends POST request and passes response to supplied handler.
-     *
-     * @param body request entity body
-     * @param headers request headers
-     * @param f response handler
-     *
-     * @return value returned from supplied handler
-     */
-    def post[T](body: Entity, headers: Header*)(f: HttpResponse => T): T =
-      request("POST", headers, Option(body))(f)
-
-    /**
-     * Sends PUT request and passes response to supplied handler.
-     *
-     * @param body request entity body
-     * @param headers request headers
-     * @param f response handler
-     *
-     * @return value returned from supplied handler
-     */
-    def put[T](body: Entity, headers: Header*)(f: HttpResponse => T): T =
-      request("PUT", headers, Option(body))(f)
-
-    /**
-     * Sends PATCH request and passes response to supplied handler.
-     *
-     * @param body request entity body
-     * @param headers request headers
-     * @param f response handler
-     *
-     * @return value returned from supplied handler
-     */
-    def patch[T](body: Entity, headers: Header*)(f: HttpResponse => T): T =
-      request("PATCH", headers, Option(body))(f)
-
-    /**
-     * Sends DELETE request and passes response to supplied handler.
-     *
-     * @param headers request headers
-     * @param f response handler
-     *
-     * @return value returned from supplied handler
-     */
-    def delete[T](headers: Header*)(f: HttpResponse => T): T =
-      request("DELETE", headers)(f)
-
-    /**
-     * Sends HEAD request and passes response to supplied handler.
-     *
-     * @param headers request headers
-     * @param f response handler
-     *
-     * @return value returned from supplied handler
-     */
-    def head[T](headers: Header*)(f: HttpResponse => T): T =
-      request("HEAD", headers)(f)
-
-    /**
-     * Sends TRACE request and passes response to supplied handler.
-     *
-     * @param headers request headers
-     * @param f response handler
-     *
-     * @return value returned from supplied handler
-     */
-    def trace[T](headers: Header*)(f: HttpResponse => T): T =
-      request("TRACE", headers)(f)
-
-    /**
-     * Sends OPTIONS request and passes response to supplied handler.
-     *
-     * @param headers request headers
-     * @param f response handler
-     *
-     * @return value returned from supplied handler
-     */
-    def options[T](headers: Header*)(f: HttpResponse => T): T =
-      request("OPTIONS", headers)(f)
 
     private def createURL(path: String, query: String): URL =
       buildURI(url.getProtocol, url.getAuthority, path, query, url.getRef).toURL
-
-    private def writeBody(conn: HttpURLConnection, body: Entity): Unit = {
-      body.length match {
-        case Some(length) =>
-          conn.setRequestProperty("Content-Length", length.toString)
-          conn.setFixedLengthStreamingMode(length)
-
-        case None =>
-          conn.setRequestProperty("Transfer-Encoding", "chunked")
-          conn.setChunkedStreamingMode(8192)
-      }
-
-      body.withInputStream { in =>
-        val out = conn.getOutputStream
-        val buf = new Array[Byte](8192)
-        var len = 0
-
-        while ({ len = in.read(buf); len != -1 })
-          out.write(buf, 0, len)
-      }
-    }
-
-    private def getHeaders(conn: HttpURLConnection): Seq[Header] = {
-      val headers = getHeaders(conn, 1, Nil)
-
-      if ("chunked".equalsIgnoreCase(conn.getHeaderField("Transfer-Encoding")))
-        headers :+ Header.parse("X-Scamper-Transfer-Decoding: chunked")
-      else headers
-    }
-
-    @tailrec
-    private def getHeaders(conn: HttpURLConnection, keyIndex: Int, headers: Seq[Header]): Seq[Header] =
-      conn.getHeaderFieldKey(keyIndex) match {
-        case null => headers
-        case key  => getHeaders(conn, keyIndex + 1, headers :+ Header(key, conn.getHeaderField(keyIndex)))
-      }
-
-    private def getBody(conn: HttpURLConnection): Entity =
-      Entity { () =>
-        if (conn.getResponseCode < 400) conn.getInputStream
-        else conn.getErrorStream
-      }
   }
 
   private def buildURI(scheme: String, authority: String, path: String, query: String, fragment: String): String = {
