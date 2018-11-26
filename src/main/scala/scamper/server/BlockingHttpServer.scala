b214235cad367a97cf80457471065eba819aeee9
Carlos Conyers <carlos.conyers@hotmail.com>
2018-11-25 22:11:15 -0500

    Added HttpServer implementation
    Removed RequestHandlerChain and RequestNotSatisfied

---
+++ src/main/scala/scamper/server/BlockingHttpServer.scala
@@ -0,0 +1,214 @@
+/*
+ * Copyright 2018 Carlos Conyers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package scamper.server
+
+import java.io.{ File, FileWriter, PrintWriter }
+import java.net.{ InetAddress, InetSocketAddress, Socket }
+import java.time.OffsetDateTime
+import java.util.concurrent.{ ArrayBlockingQueue, RejectedExecutionHandler, TimeUnit, ThreadFactory, ThreadPoolExecutor }
+import java.util.concurrent.atomic.AtomicInteger
+
+import javax.net.ServerSocketFactory
+import javax.net.ssl.{ SSLException, SSLServerSocketFactory }
+
+import scala.collection.mutable.ArrayBuffer
+import scala.concurrent.{ ExecutionContext, Future }
+import scala.util.Try
+
+import scamper.{ Header, HttpRequest, HttpResponse, RequestLine }
+import scamper.ImplicitConverters.{ inputStreamToEntity, stringToEntity }
+import scamper.ResponseStatuses.NotFound
+import scamper.auxiliary.SocketType
+import scamper.headers.{ Connection, ContentLength, ContentType, TransferEncoding }
+import scamper.types.ImplicitConverters.stringToTransferCoding
+
+private object BlockingHttpServer {
+  private val count = new AtomicInteger(0)
+
+  case class Configuration(
+    poolSize: Int = Runtime.getRuntime.availableProcessors(),
+    queueSize: Int = Runtime.getRuntime.availableProcessors() * 4,
+    readTimeout: Int = 5000,
+    keepAliveSeconds: Int = 60,
+    log: File = new File("server.log"),
+    handlers: Seq[RequestHandler] = Nil,
+    factory: ServerSocketFactory = ServerSocketFactory.getDefault()
+  )
+
+  def apply(host: InetAddress, port: Int, config: Configuration) =
+    new BlockingHttpServer(count.incrementAndGet(), host, port, config)
+}
+
+private class BlockingHttpServer private(val id: Int, val host: InetAddress, val port: Int, config: BlockingHttpServer.Configuration) extends HttpServer {
+  private val authority = s"${host.getCanonicalHostName}:$port"
+  private val threadGroup = new ThreadGroup(s"httpserver-$id")
+  private val poolSize = config.poolSize
+  private val queueSize = config.queueSize
+  private val keepAliveSeconds = config.keepAliveSeconds
+  private val handlers = config.handlers
+  private val logger = new PrintWriter(new FileWriter(config.log, true), true)
+  private val serverSocket = config.factory.createServerSocket()
+  private var closed = false
+
+  private implicit val executor = ExecutionContext.fromExecutorService {
+    object ServiceUnavailableHandler extends RejectedExecutionHandler {
+      def rejectedExecution(task: Runnable, executor: ThreadPoolExecutor): Unit = log("[error] Task rejected")
+    }
+
+    object ServiceThreadFactory extends ThreadFactory {
+      private val count = new AtomicInteger(0)
+      def newThread(task: Runnable) = new Thread(threadGroup, task, s"httpserver-$id-service-${count.incrementAndGet()}")
+    }
+
+    val queue = new ArrayBlockingQueue[Runnable](queueSize)
+    new ThreadPoolExecutor(poolSize, poolSize, keepAliveSeconds, TimeUnit.SECONDS, queue, ServiceThreadFactory, ServiceUnavailableHandler)
+  }
+
+  val isSecure: Boolean = config.factory.isInstanceOf[SSLServerSocketFactory]
+
+  def isClosed: Boolean = synchronized(closed)
+
+  def close(): Unit = synchronized {
+    if (!closed) {
+      Try(log("[info] Shutting down server"))
+      Try(serverSocket.close())
+      Try(executor.shutdownNow())
+      Try(logger.close())
+      closed = true
+    }
+  }
+
+  override def toString(): String = s"HttpServer(host=$host, port=$port, isSecure=$isSecure, isClosed=$isClosed)"
+
+  try {
+    log(s"[info] Starting server at $authority")
+    serverSocket.bind(new InetSocketAddress(host, port), queueSize)
+    Service.start()
+    log(s"[info] Server running at $authority")
+  } catch {
+    case cause: Exception =>
+      log(s"[error] Failed to start server at $authority", Some(cause))
+      close()
+      throw cause
+  }
+
+  private def log(message: String, cause: Option[Throwable] = None): Unit = {
+    logger.printf("%s [%s]%s%n", OffsetDateTime.now(), authority, message)
+    cause.foreach(_.printStackTrace(logger))
+  }
+
+  private object Service extends Thread(threadGroup, s"httpserver-$id-service") {
+    override def run(): Unit = {
+      while (!isClosed)
+        try
+          service(serverSocket.accept())
+        catch {
+          case _ if !isClosed => // Ignore if server is closed
+
+          case cause: Exception =>
+            if (serverSocket.isClosed) {
+              log(s"[error] Error while waiting for connection: $cause", Some(cause))
+              Try(close())
+            } else {
+              log(s"[warning] Error while waiting for connection: $cause", Some(cause))
+            }
+        }
+    }
+
+    private def service(implicit socket: Socket): Unit = {
+      log(s"[info] Connection received from ${format(socket)}")
+
+      Future {
+        try {
+          log(s"[info] Servicing request from ${format(socket)}")
+          socket.setSoTimeout(config.readTimeout)
+
+          val init: Either[HttpRequest, HttpResponse] = Left(read())
+
+          val res = handlers.foldLeft(init) { (result, handler) =>
+            result.left.flatMap(req => handler(req))
+          } match {
+            case Right(res) => res
+            case Left(req)  => NotFound()
+          }
+
+          write(getEffectiveResponse(res))
+          log(s"[info] Response sent to ${format(socket)}")
+        } catch {
+          case cause: Exception =>
+            log(s"[error] Error while servicing request from ${format(socket)}", Some(cause))
+        } finally {
+            log(s"[info] Closing connection to ${format(socket)}")
+            Try(socket.close())
+        }
+      }
+    }
+
+    private def format(socket: Socket): String =
+      socket.getInetAddress.getHostAddress + ":" + socket.getPort
+
+    private def read()(implicit socket: Socket): HttpRequest = {
+      val buffer = new Array[Byte](8192)
+      val startLine = RequestLine.parse(socket.readLine(buffer))
+      val headers = new ArrayBuffer[Header](8)
+      var line = ""
+
+      while ({ line = socket.readLine(buffer); line != "" })
+        headers += Header.parse(line)
+
+      HttpRequest(startLine, headers.toSeq, socket.getInputStream)
+    }
+
+    private def write(res: HttpResponse)(implicit socket: Socket): Unit = {
+      socket.writeLine(res.startLine.toString)
+      res.headers.map(_.toString).foreach(socket.writeLine)
+      socket.writeLine()
+
+      if (!res.body.isKnownEmpty) {
+        val in = res.body.getInputStream
+        val buffer = new Array[Byte](8192)
+        var length = 0
+
+        res.getTransferEncoding.map { _ =>
+          while ({ length = in.read(buffer); length != -1 }) {
+            socket.writeLine(length.toHexString)
+            socket.write(buffer, 0, length)
+            socket.writeLine()
+          }
+
+          socket.writeLine("0")
+          socket.writeLine()
+        }.getOrElse {
+          while ({ length = in.read(buffer); length != -1 })
+            socket.write(buffer, 0, length)
+        }
+      }
+
+      socket.flush()
+    }
+
+    private def getEffectiveResponse(res: HttpResponse): HttpResponse = {
+      if (res.hasTransferEncoding || res.hasContentLength)
+        res
+      else
+        res.body.length match {
+          case Some(0) => res.getContentType.map(_ => res.withContentLength(0)).getOrElse(res)
+          case Some(n) => res.withContentLength(n)
+          case None    => res.withTransferEncoding("chunked")
+        }
+    }.withConnection("close")
+  }
+}
--- src/main/scala/scamper/server/package.scala
+++ src/main/scala/scamper/server/package.scala
@@ -15,7 +15,10 @@
  */
 package scamper
 
+import java.io.File
 import java.net.InetAddress
+
+import javax.net.ServerSocketFactory
 
 /** Includes server related items. */
 package object server {
@@ -60,33 +63,11 @@ package object server {
       Right(process(request))
   }
 
-  /** Indicates no response was generated for given request. */
-  case class RequestNotSatisfied(request: HttpRequest) extends HttpException
-
-  /** Provides utility for applying chain of request handlers to request. */
-  object RequestHandlerChain {
-    /**
-     * Sends request through chain of request handlers in search of response.
-     *
-     * The chain is broken upon first generated response. That is, the handlers
-     * are invoked up to first generated response, and the remaining handlers are
-     * bypassed.
-     *
-     * @throws RequestNotSatisfied if no response is generated
-     */
-    def getResponse(request: HttpRequest, handlers: Seq[RequestHandler]): HttpResponse = {
-      val init: Either[HttpRequest, HttpResponse] = Left(request)
-
-      handlers.foldLeft(init) { (prev, handler) =>
-        prev.fold(req => handler(req), res => Right(res))
-      } match {
-        case Right(response) => response
-        case Left(request) => throw RequestNotSatisfied(request)
-      }
-    }
-  }
-
-  /** HTTP Server */
+  /**
+   * HTTP Server
+   *
+   * @see [[HttpServer$ HttpServer]], [[ServerConfiguration]]
+   */
   trait HttpServer {
     /** Gets host address. */
     def host: InetAddress
@@ -106,5 +87,265 @@ package object server {
      * @return `true` if server is closed; `false` otherwise
      */
     def isClosed: Boolean
+  }
+
+  /** Provides factory for configuring and creating `HttpServer`. */
+  object HttpServer {
+    /** Gets default server configuration. */
+    def configure(): ServerConfiguration = new ServerConfiguration()
+
+    /**
+     * Creates `HttpServer` at given port with default configuration and
+     * supplied processor.
+     *
+     * @param port port number
+     * @param processor request processor
+     *
+     * @return server
+     */
+    def create(port: Int)(processor: RequestProcessor): HttpServer =
+      create(InetAddress.getLocalHost(), port)(processor)
+
+    /**
+     * Creates `HttpServer` at given host and port with default configuration
+     * and supplied processor.
+     *
+     * @param host host address
+     * @param port port number
+     * @param processor request processor
+     *
+     * @return server
+     */
+    def create(host: String, port: Int)(processor: RequestProcessor): HttpServer =
+      create(InetAddress.getByName(host), port)(processor)
+
+    /**
+     * Creates `HttpServer` at given host and port with default configuration
+     * and supplied processor.
+     *
+     * @param host host address
+     * @param port port number
+     * @param processor request processor
+     *
+     * @return server
+     */
+    def create(host: InetAddress, port: Int)(processor: RequestProcessor): HttpServer =
+      configure().include(processor).create(host, port)
+  }
+
+  /**
+   * Used to configure and create `HttpServer`.
+   *
+   * `ServerConfiguration` is a mutable, thread-safe structure. With each
+   * requested change, the configuration is modified and returned. Changes
+   * applied after creating server are not effected in server.
+   *
+   * @constructor Creates default server configuration.
+   *
+   * === Default Configuration ===
+   *
+   * |Key          |Value |
+   * |-------------|------|
+   * |poolSize     |`Runtime.getRuntime().availableProcessors()`|
+   * |queueSize    |`Runtime.getRuntime().availableProcessors() * 4`|
+   * |readTimeout  |`5000`|
+   * |log          |`new File("server.log")`|
+   * |secure       |<em>(non-secure)</em>|
+   * |include      |<em>(no handlers)</em>|
+   */
+  class ServerConfiguration {
+    private var config = BlockingHttpServer.Configuration()
+
+    /** Resets configuration to default values. */
+    def reset(): this.type = synchronized {
+      config = BlockingHttpServer.Configuration()
+      this
+    }
+
+    /**
+     * Sets queue size.
+     *
+     * @param size queue size
+     *
+     * @return this configuration
+     */
+    def queueSize(size: Int): this.type = synchronized {
+      config = config.copy(queueSize = size)
+      this
+    }
+
+    /**
+     * Sets pool size.
+     *
+     * @param size pool size
+     *
+     * @return this configuration
+     */
+    def poolSize(size: Int): this.type = synchronized {
+      config = config.copy(poolSize = size)
+      this
+    }
+
+    /**
+     * Sets read timeout.
+     *
+     * @param timeout read timeout in milliseconds
+     *
+     * @return this configuration
+     */
+    def readTimeout(timeout: Int): this.type = synchronized {
+      config = config.copy(readTimeout = timeout)
+      this
+    }
+
+    /**
+     * Sets log to given file.
+     *
+     * @param file log file
+     *
+     * @return this configuration
+     */
+    def log(file: File): this.type = synchronized {
+      config = config.copy(log = file)
+      this
+    }
+
+    /**
+     * Sets protocol to `http`.
+     *
+     * @return this configuration
+     */
+    def nonSecure(): this.type = synchronized {
+      config = config.copy(factory = ServerSocketFactory.getDefault())
+      this
+    }
+
+    /**
+     * Sets protocol to `https`.
+     *
+     * When server is created it will be configured to use supplied key store.
+     *
+     * @param keyStore server key store
+     * @param password key store password
+     * @param storeType key store type (i.e., JKS, JCEKS, etc.)
+     *
+     * @return this configuration
+     */
+    def secure(keyStore: File, password: String, storeType: String): this.type = synchronized {
+      config = config.copy(factory = SecureServerSocketFactory.create(keyStore, password.toCharArray, storeType))
+      this
+    }
+
+    /**
+     * Sets protocol to `https`.
+     *
+     * When server is created it will be configured to use supplied key store.
+     *
+     * <strong>Note:</strong> The password can be discarded after invoking this
+     * method.
+     *
+     * @param keyStore server key store
+     * @param password key store password
+     * @param storeType key store type (i.e., JKS, JCEKS, etc.)
+     *
+     * @return this configuration
+     */
+    def secure(keyStore: File, password: Array[Char], storeType: String): this.type = synchronized {
+      config = config.copy(factory = SecureServerSocketFactory.create(keyStore, password, storeType))
+      this
+    }
+
+    /**
+     * Sets protocol to `https`.
+     *
+     * When server is created it will be configured to use supplied key and
+     * certificate.
+     *
+     * @param key private key
+     * @param certificate public key certificate
+     *
+     * @return this configuration
+     */
+    def secure(key: File, certificate: File): this.type = synchronized {
+      config = config.copy(factory = SecureServerSocketFactory.create(key, certificate))
+      this
+    }
+
+    /**
+     * Includes supplied request handler.
+     *
+     * The handler is appended to existing request handler chain.
+     *
+     * @return this configuration
+     */
+    def include(handler: RequestHandler): this.type = synchronized {
+      config = config.copy(handlers = config.handlers :+ handler)
+      this
+    }
+
+    /**
+     * Includes supplied request filter.
+     *
+     * The filter is appended to existing request handler chain.
+     *
+     * @return this configuration
+     */
+    def include(filter: RequestFilter): this.type = synchronized {
+      config = config.copy(handlers = config.handlers :+ filter)
+      this
+    }
+
+    /**
+     * Includes supplied request processor.
+     *
+     * The processor is appended to existing request handler chain.
+     *
+     * @return this configuration
+     */
+    def include(processor: RequestProcessor): this.type = synchronized {
+      config = config.copy(handlers = config.handlers :+ processor)
+      this
+    }
+
+    /**
+     * Creates `HttpServer` at given port.
+     *
+     * @param port port number
+     *
+     * @return new server
+     */
+    def create(port: Int): HttpServer = synchronized {
+      create(InetAddress.getLocalHost(), port)
+    }
+
+    /**
+     * Creates `HttpServer` at given host and port.
+     *
+     * @param host host address
+     * @param port port number
+     *
+     * @return new server
+     */
+    def create(host: String, port: Int): HttpServer = synchronized {
+      create(InetAddress.getByName(host), port)
+    }
+
+    /**
+     * Creates `HttpServer` at given host and port.
+     *
+     * @param host host address
+     * @param port port number
+     *
+     * @return new server
+     */
+    def create(host: InetAddress, port: Int): HttpServer = synchronized {
+      BlockingHttpServer(host, port, config)
+    }
+  }
+
+  /** Provides factory for `ServerConfiguration`. */
+  object ServerConfiguration {
+    /** Creates default `ServerConfiguration`. */
+    def apply(): ServerConfiguration = new ServerConfiguration()
   }
 }
