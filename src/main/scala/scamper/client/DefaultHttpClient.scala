dc945c7e44edc8d4845106088c04ac72c0d7b96f
Carlos Conyers <carlos.conyers@hotmail.com>
2019-02-23 16:27:07 -0500

    Updated user-agent header

--- src/main/scala/scamper/client/DefaultHttpClient.scala
+++ src/main/scala/scamper/client/DefaultHttpClient.scala
@@ -52,7 +52,7 @@ private class DefaultHttpClient private (val bufferSize: Int, val readTimeout: I
 
     val secure = target.getScheme == "https"
     val host = getEffectiveHost(target)
-    val userAgent = request.getHeaderValueOrElse("User-Agent", "Scamper/5.0.2")
+    val userAgent = request.getHeaderValueOrElse("User-Agent", "Scamper/5.1")
     val connection = getEffectiveConnection(request)
 
     var effectiveRequest = request.method match {
