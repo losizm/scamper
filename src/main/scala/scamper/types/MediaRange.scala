b5bd5efbb74c341a91d7de12c04950390fb2c742
Carlos Conyers <carlos.conyers@hotmail.com>
2019-05-19 19:26:22 -0400

    Updated license info

--- src/main/scala/scamper/types/MediaRange.scala
+++ src/main/scala/scamper/types/MediaRange.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Carlos Conyers
+ * Copyright 2019 Carlos Conyers
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
