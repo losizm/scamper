cd1c53b66299568c822b50efd11296fc44b29429
Carlos Conyers <carlos.conyers@hotmail.com>
2019-09-29 16:49:02 -0400

    Promoted to 10.0.2

--- build.sbt
+++ build.sbt
@@ -1,6 +1,6 @@
 organization := "com.github.losizm"
 name := "scamper"
-version := "10.0.1"
+version := "10.0.2"
 
 description := "The HTTP library for Scala"
 homepage := Some(url("https://github.com/losizm/scamper"))
--- src/main/scala/scamper/client/DefaultHttpClient.scala
+++ src/main/scala/scamper/client/DefaultHttpClient.scala
@@ -57,7 +57,7 @@ private class DefaultHttpClient private (val bufferSize: Int, val readTimeout: I
 
     val secure = target.getScheme == "https"
     val host = getEffectiveHost(target)
-    val userAgent = request.getHeaderValueOrElse("User-Agent", "Scamper/10.0.1")
+    val userAgent = request.getHeaderValueOrElse("User-Agent", "Scamper/10.0.2")
     val connection = getEffectiveConnection(request)
 
     var effectiveRequest = request.method match {
