7c9e21ee1939665396dd73d0e8dd2600f0d76aef
Carlos Conyers <carlos.conyers@hotmail.com>
2019-10-26 01:51:08 -0400

    Moved HttpRequest and HttpResponse implementations to separate source files

---
+++ src/main/scala/scamper/HttpRequestImpl.scala
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2019 Carlos Conyers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package scamper
+
+import java.net.URI
+import Auxiliary.UriType
+
+private case class HttpRequestImpl(startLine: RequestLine, headers: Seq[Header], body: Entity, attributes: Map[String, Any] = Map.empty) extends HttpRequest {
+  lazy val path: String = target.normalize.getRawPath match {
+    case "" =>
+      if (method.name == "OPTIONS")
+        "*"
+      else
+        "/"
+    case path => path
+  }
+
+  lazy val query: QueryString =
+    target.getRawQuery match {
+      case null  => QueryString.empty
+      case query => QueryString(query)
+    }
+
+  def withStartLine(newStartLine: RequestLine): HttpRequest =
+    copy(startLine = newStartLine)
+
+  def withMethod(newMethod: RequestMethod): HttpRequest =
+    copy(startLine = RequestLine(newMethod, target, version))
+
+  def withTarget(newTarget: URI): HttpRequest =
+    copy(startLine = RequestLine(method, newTarget, version))
+
+  def withPath(newPath: String): HttpRequest =
+    newPath match {
+      case "*" if method.name == "OPTIONS" => withTarget(target.withPath(""))
+      case _   => withTarget(target.withPath(newPath))
+    }
+
+  def withQuery(query: QueryString): HttpRequest =
+    withTarget(target.withQuery(query.toString))
+
+  def withQuery(params: Map[String, Seq[String]]): HttpRequest =
+    withQuery(QueryString(params))
+
+  def withQuery(params: (String, String)*): HttpRequest =
+    withQuery(QueryString(params : _*))
+
+  def withVersion(newVersion: HttpVersion): HttpRequest =
+    copy(startLine = RequestLine(method, target, newVersion))
+
+  def withHeaders(newHeaders: Header*): HttpRequest =
+    copy(headers = newHeaders)
+
+  def addHeaders(newHeaders: Header*): HttpRequest =
+    withHeaders(headers ++ newHeaders : _*)
+
+  def removeHeaders(names: String*): HttpRequest =
+    withHeaders(headers.filterNot(header => names.exists(header.name.equalsIgnoreCase)) : _*)
+
+  def withHeader(header: Header): HttpRequest =
+    withHeaders(headers.filterNot(_.name.equalsIgnoreCase(header.name)) :+ header : _*)
+
+  def withBody(newBody: Entity): HttpRequest =
+    copy(body = newBody)
+
+  def withAttributes(newAttributes: Map[String, Any]): HttpRequest =
+    copy(attributes = newAttributes)
+
+  def withAttribute(attribute: (String, Any)): HttpRequest =
+    copy(attributes = attributes + attribute)
+
+  def removeAttribute(name: String): HttpRequest =
+    copy(attributes = attributes - name)
+}
---
+++ src/main/scala/scamper/HttpResponseImpl.scala
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2019 Carlos Conyers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package scamper
+
+private case class HttpResponseImpl(startLine: StatusLine, headers: Seq[Header], body: Entity, attributes: Map[String, Any] = Map.empty) extends HttpResponse {
+  def withStartLine(newStartLine: StatusLine) =
+    copy(startLine = newStartLine)
+
+  def withStatus(newStatus: ResponseStatus): HttpResponse =
+    copy(startLine = StatusLine(newStatus, version))
+
+  def withVersion(newVersion: HttpVersion): HttpResponse =
+    copy(startLine = StatusLine(status, newVersion))
+
+  def withHeaders(newHeaders: Header*): HttpResponse =
+    copy(headers = newHeaders)
+
+  def addHeaders(newHeaders: Header*): HttpResponse =
+    withHeaders(headers ++ newHeaders : _*)
+
+  def removeHeaders(names: String*): HttpResponse =
+    withHeaders(headers.filterNot(header => names.exists(header.name.equalsIgnoreCase)) : _*)
+
+  def withHeader(header: Header): HttpResponse =
+    withHeaders(headers.filterNot(_.name.equalsIgnoreCase(header.name)) :+ header : _*)
+
+  def withBody(newBody: Entity): HttpResponse =
+    copy(body = newBody)
+
+  def withAttributes(newAttributes: Map[String, Any]): HttpResponse =
+    copy(attributes = newAttributes)
+
+  def withAttribute(attribute: (String, Any)): HttpResponse =
+    copy(attributes = attributes + attribute)
+
+  def removeAttribute(name: String): HttpResponse =
+    copy(attributes = attributes - name)
+}
